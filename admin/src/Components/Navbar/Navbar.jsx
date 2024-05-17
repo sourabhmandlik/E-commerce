9e5304c76bf742f1af29b78edc814d1e72c63d8c
sourabh mandlik <sourabh3393@gmail.com>
2024-05-17 16:09:56 +0530

    my first commit

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={
+            navlogo
+        } alt="" className='"nav-log'>
+        
+        </img>
+        <img src={navProfile} alt="" className='nav-profile'>
+        </img>
+    </div>
+  )
+}
+
+export default Navbar
